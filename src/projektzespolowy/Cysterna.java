ac21d8d8d9cd3be4e5de7b2046e2c3a2d8e911eb
Pasterz <mateusz.ppasternak@gmail.com>
2016-05-22 13:37:24 +0200

    Test 01 - zakonczony pomyslnie Test02 - w trakcie

--- src/projektzespolowy/Cysterna.java
+++ src/projektzespolowy/Cysterna.java
@@ -10,31 +10,48 @@ package projektzespolowy;
  * @author PasterzM
  */
 public class Cysterna {
+
     public final int MAX_KOMOR = 5;
     public final int MAX_POJEMNOSC_KOMOR = 10;
     public int dlugosc_trasy;
-   //private String nazwa_cysterny;
+    //private String nazwa_cysterny;
     public Komora[] komora = new Komora[MAX_KOMOR];
-    
-    Cysterna()
-    {
-        for(int i=0 ; i<komora.length ;i++){
+    public Komora2[] komora2 = new Komora2[MAX_KOMOR];
+
+    Cysterna() {
+        for (int i = 0; i < komora.length; i++) {
             komora[i] = new Komora();
         }
+        for (int i = 0; i < komora.length; i++) {
+            komora2[i] = new Komora2();
+        }
     }
-    
-    public class Komora{
+
+    public class Komora {
+
         public int pojemnosc;
         String nazwa_paliwa = new String("");
         char nazwa_stacji;
-        
-        public Komora()
-        {
-            this.pojemnosc=0;
-            this.nazwa_paliwa=new String("");
-            this.nazwa_stacji=' ';
+
+        public Komora() {
+            this.pojemnosc = 0;
+            this.nazwa_paliwa = new String("");
+            this.nazwa_stacji = ' ';
         }
-        
-        
+
+    };
+
+    public class Komora2 {
+
+        public int pojemnosc;
+        public String nazwa_paliwa = new String("");
+        public char[] nazwa_stacji;
+
+        public Komora2() {
+            this.pojemnosc = 0;
+            this.nazwa_paliwa = new String("");
+            //this.nazwa_stacji= new char[OknoInicjalizujace.iloscStacji];
+            this.nazwa_stacji = new char[6]; //dle testow potem zamienic
+        }
     };
 }
--- src/projektzespolowy/Wyniki.java
+++ src/projektzespolowy/Wyniki.java
@@ -11,20 +11,43 @@ package projektzespolowy;
 public class Wyniki {
     private int dlugosc_trasy;  //suma dlugosci wszystkich tras przez ktore ma pzrejechac cysterna
     private Stacja stacje;  //stacje przez ktore musi przejechac cysterna
-    /*
-    Wyniki(Cysterna cys)
-    {
-        this.dlugosc_trasy = cys.dlugosc_trasy;
+/*    
+    private int wynik_pb95;
+    private int wynik_98;
+    private int wynik_on;
+    private int wynik_on_s;
+    private int wynik_on_eko;
+  */  
+    Wyniki(Cysterna cys){
+        stacje = new Stacja();
+        stacje.set_nazwa(cys.komora[0].nazwa_stacji);
+        dlugosc_trasy = cys.dlugosc_trasy;
+        //this.dlugosc_trasy = cys.dlugosc_trasy;
         for(int i=0;    i<cys.komora.length;    i++)
         {
             if(cys.komora[i].nazwa_paliwa.equals("PB95"))
             {
-                stacje.setZap_pb95(cys.komora[i].);
+                stacje.setZap_pb95( stacje.getZap_95() + cys.komora[i].pojemnosc);
             }
-                    
-        this.stacje.zap_pb95 = cys.komora[]
+            if(cys.komora[i].nazwa_paliwa.equals("PB98"))
+            {
+                stacje.setZap_98(stacje.getZap_98() + cys.komora[i].pojemnosc);
+            }
+            if(cys.komora[i].nazwa_paliwa.equals("ON"))
+            {
+                stacje.setZap_on(stacje.getZap_on()+ cys.komora[i].pojemnosc);
+            }
+            if(cys.komora[i].nazwa_paliwa.equals("ONs"))
+            {
+                stacje.setZap_on_s(stacje.getZap_on_s() + cys.komora[i].pojemnosc);
+            }
+            if(cys.komora[i].nazwa_paliwa.equals("ONeko"))
+            {
+                stacje.setZap_on_eko(stacje.getZap_on_eko()+ cys.komora[i].pojemnosc);
+            }            
         }
     }
-    */
-    
+   public void wyswietl_wynik(){
+       System.out.println(stacje.getNazwa()+" "+dlugosc_trasy+" "+stacje.getZap_95()+" "+stacje.getZap_98()+" "+stacje.getZap_on()+" "+stacje.getZap_on_eko()+" "+stacje.getZap_on_s());
+   }
 }
